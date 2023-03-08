41c6248ca75844aa2e8d0974a4994c2a85f96f10
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-03-08 11:29:30 +0100

    refactor: move functions into its own file

--- bin/initialize.ts
+++ bin/initialize.ts
@@ -1,161 +1,25 @@
 import {
-	AttachPolicyCommand,
-	CreateKeysAndCertificateCommand,
-	CreatePolicyCommand,
-	DescribeEndpointCommand,
-	IoTClient,
-} from '@aws-sdk/client-iot'
+	generateIotCredentials,
+	getIotCredentialsSSM,
+	getIoTEndpoint,
+	saveIotCredentialsSSM,
+} from './lib/iot.js'
+
 import {
-	GetParameterCommand,
-	ParameterType,
-	PutParameterCommand,
-	SSMClient,
-} from '@aws-sdk/client-ssm'
-import { randomUUID } from 'crypto'
-import needle from 'needle'
-import {
-	IOT_CERT_PARAM,
-	IOT_ENDPOINT_PARAM,
-	IOT_KEY_PARAM,
-	NRFCLOUD_ACCOUNT_INFO_PARAM,
-	NRFCLOUD_CLIENT_CERT_PARAM,
-	NRFCLOUD_CLIENT_KEY_PARAM,
-} from '../cdk/stacks/stackConfig'
+	AccountInfo,
+	deleteNrfcloudCredentials,
+	generateNrfcloudCredentials,
+	getAccountInfo,
+	getNrfcloudCredentialsSSM,
+	saveNrfcloudAccountInfosSSM,
+	saveNrfcloudCredentialsSSM,
+} from './lib/nrfcloud.js'
 
 type CliInput = {
 	apiKey: string
 	reset: boolean
 	endpoint: string
 }
-
-type CertificateCredentials = {
-	clientCert: string
-	privateKey: string
-}
-
-type AccountInfo = {
-	mqttEndpoint: string
-	mqttTopicPrefix: string
-	tenantId: string
-	accountDeviceClientId: string
-}
-
-type Nullable<T> = {
-	[K in keyof T]: T[K] | undefined | null
-}
-
-const Iot = new IoTClient({})
-const SSM = new SSMClient({})
-
-async function getAccountInfo({
-	apiKey,
-	endpoint,
-}: CliInput): Promise<AccountInfo> {
-	const accountInfo = await needle('get', `${endpoint}/v1/account`, {
-		headers: {
-			Authorization: `Bearer ${apiKey}`,
-		},
-		json: true,
-	})
-	const tenantId = accountInfo.body.mqttTopicPrefix.split('/')[1]
-	return {
-		mqttEndpoint: accountInfo.body.mqttEndpoint,
-		mqttTopicPrefix: accountInfo.body.mqttTopicPrefix,
-		tenantId,
-		accountDeviceClientId: `account-${tenantId}`,
-	}
-}
-
-async function getIoTEndpoint(): Promise<string | undefined> {
-	const result = await Iot.send(
-		new DescribeEndpointCommand({ endpointType: 'iot:Data-ATS' }),
-	)
-
-	return result.endpointAddress
-}
-
-async function getNrfcloudCredentialsSSM(): Promise<
-	Nullable<CertificateCredentials>
-> {
-	const nrfcloudClientCertResponse = await SSM.send(
-		new GetParameterCommand({
-			Name: NRFCLOUD_CLIENT_CERT_PARAM,
-		}),
-	).catch(() => null)
-	const nrfcloudClientKeyResponse = await SSM.send(
-		new GetParameterCommand({
-			Name: NRFCLOUD_CLIENT_KEY_PARAM,
-		}),
-	).catch(() => null)
-
-	return {
-		clientCert: nrfcloudClientCertResponse?.Parameter?.Value,
-		privateKey: nrfcloudClientKeyResponse?.Parameter?.Value,
-	}
-}
-
-async function generateNrfcloudCredentials(
-	input: CliInput,
-): Promise<CertificateCredentials> {
-	const accountDevice = await needle(
-		'post',
-		`${input.endpoint}/v1/devices/account`,
-		null,
-		{
-			json: true,
-			headers: {
-				Authorization: `Bearer ${input.apiKey}`,
-			},
-		},
-	)
-
-	return {
-		clientCert: accountDevice.body.clientCert,
-		privateKey: accountDevice.body.privateKey,
-	}
-}
-
-async function deleteNrfcloudCredentials(input: CliInput): Promise<void> {
-	await needle('delete', `${input.endpoint}/v1/devices/account`, null, {
-		json: true,
-		headers: {
-			Authorization: `Bearer ${input.apiKey}`,
-		},
-	})
-}
-
-async function saveNrfcloudCredentialsSSM(credentials: CertificateCredentials) {
-	await Promise.all([
-		SSM.send(
-			new PutParameterCommand({
-				Name: NRFCLOUD_CLIENT_CERT_PARAM,
-				Value: credentials.clientCert,
-				Type: ParameterType.STRING,
-				Overwrite: true,
-			}),
-		),
-		SSM.send(
-			new PutParameterCommand({
-				Name: NRFCLOUD_CLIENT_KEY_PARAM,
-				Value: credentials.privateKey,
-				Type: ParameterType.STRING,
-				Overwrite: true,
-			}),
-		),
-	])
-}
-
-async function saveNrfcloudAccountInfosSSM(info: AccountInfo) {
-	await SSM.send(
-		new PutParameterCommand({
-			Name: NRFCLOUD_ACCOUNT_INFO_PARAM,
-			Value: JSON.stringify(info),
-			Type: ParameterType.STRING,
-			Overwrite: true,
-		}),
-	)
-}
-
 async function ensureNrfcloudCredentials(
 	input: CliInput,
 	accountInfo: AccountInfo,
@@ -171,94 +35,10 @@ async function ensureNrfcloudCredentials(
 			saveNrfcloudCredentialsSSM(credentials),
 			saveNrfcloudAccountInfosSSM(accountInfo),
 		])
-		console.log(
-			`Saved new account device credentials to ${NRFCLOUD_CLIENT_CERT_PARAM} and ${NRFCLOUD_CLIENT_KEY_PARAM}`,
-		)
+		console.log(`Saved new account device credentials`)
 	} else {
-		console.log(
-			`Existing account device credentials were present in ${NRFCLOUD_CLIENT_CERT_PARAM} and ${NRFCLOUD_CLIENT_KEY_PARAM}`,
-		)
+		console.log(`Existing account device credentials were present, reuse them`)
 	}
-}
-
-async function getIotCredentialsSSM(): Promise<CertificateCredentials> {
-	const iotClientCertResponse = await SSM.send(
-		new GetParameterCommand({
-			Name: IOT_CERT_PARAM,
-		}),
-	).catch(() => null)
-	const iotClientKeyResponse = await SSM.send(
-		new GetParameterCommand({
-			Name: IOT_KEY_PARAM,
-		}),
-	).catch(() => null)
-
-	return {
-		clientCert: iotClientCertResponse?.Parameter?.Value ?? '',
-		privateKey: iotClientKeyResponse?.Parameter?.Value ?? '',
-	}
-}
-
-async function generateIotCredentials(): Promise<CertificateCredentials> {
-	const policyName = `nrfcloud-mqtt-bridge-policy-${new Date()
-		.toISOString()
-		.replace(/T.+$/, '')}-${randomUUID()}`
-	console.log(`Creating iot policy ${policyName}`)
-	await Iot.send(
-		new CreatePolicyCommand({
-			policyDocument:
-				'{"Version": "2012-10-17","Statement": [{"Effect": "Allow","Action": "iot:*","Resource": "*"}]}',
-			policyName,
-		}),
-	)
-	const credentials = await Iot.send(
-		new CreateKeysAndCertificateCommand({
-			setAsActive: true,
-		}),
-	)
-	await Iot.send(
-		new AttachPolicyCommand({
-			policyName,
-			target: credentials.certificateArn,
-		}),
-	)
-
-	return {
-		clientCert: credentials.certificatePem ?? '',
-		privateKey: credentials.keyPair?.PrivateKey ?? '',
-	}
-}
-
-async function saveIotCredentialsSSM(
-	credentials: CertificateCredentials,
-	iotEndpoint: string,
-): Promise<void> {
-	await Promise.all([
-		SSM.send(
-			new PutParameterCommand({
-				Name: IOT_ENDPOINT_PARAM,
-				Value: iotEndpoint,
-				Type: ParameterType.STRING,
-				Overwrite: true,
-			}),
-		),
-		SSM.send(
-			new PutParameterCommand({
-				Name: IOT_CERT_PARAM,
-				Value: credentials.clientCert,
-				Type: ParameterType.STRING,
-				Overwrite: true,
-			}),
-		),
-		SSM.send(
-			new PutParameterCommand({
-				Name: IOT_KEY_PARAM,
-				Value: credentials.privateKey,
-				Type: ParameterType.STRING,
-				Overwrite: true,
-			}),
-		),
-	])
 }
 
 async function ensureIotCredentials(input: CliInput, iotEndpoint: string) {
@@ -267,13 +47,9 @@ async function ensureIotCredentials(input: CliInput, iotEndpoint: string) {
 		console.log('Generating new IoT credentials')
 		const credentials = await generateIotCredentials()
 		await saveIotCredentialsSSM(credentials, iotEndpoint)
-		console.log(
-			`Saved new iot credentials to ${IOT_CERT_PARAM} and ${IOT_KEY_PARAM}`,
-		)
+		console.log(`Saved new iot credentials`)
 	} else {
-		console.log(
-			`Existing iot credentials were present in ${IOT_CERT_PARAM} and ${IOT_KEY_PARAM}`,
-		)
+		console.log(`Existing iot credentials were present, reuse them`)
 	}
 }
 
---
+++ bin/lib/nrfcloud.ts
@@ -0,0 +1,156 @@
+import {
+	GetParameterCommand,
+	ParameterType,
+	PutParameterCommand,
+	SSMClient,
+} from '@aws-sdk/client-ssm'
+import needle from 'needle'
+import {
+	NRFCLOUD_ACCOUNT_INFO_PARAM,
+	NRFCLOUD_CLIENT_CERT_PARAM,
+	NRFCLOUD_CLIENT_KEY_PARAM,
+	STACK_NAME,
+} from '../../cdk/stacks/stackConfig'
+
+export type CertificateCredentials = {
+	clientCert: string
+	privateKey: string
+}
+
+export type AccountInfo = {
+	mqttEndpoint: string
+	mqttTopicPrefix: string
+	tenantId: string
+	accountDeviceClientId: string
+}
+
+type Nullable<T> = {
+	[K in keyof T]: T[K] | undefined | null
+}
+
+const SSM = new SSMClient({})
+
+async function getAccountInfo({
+	apiKey,
+	endpoint,
+}: {
+	apiKey: string
+	endpoint: string
+}): Promise<AccountInfo> {
+	const accountInfo = await needle('get', `${endpoint}/v1/account`, {
+		headers: {
+			Authorization: `Bearer ${apiKey}`,
+		},
+		json: true,
+	})
+	const tenantId = accountInfo.body.mqttTopicPrefix.split('/')[1]
+	return {
+		mqttEndpoint: accountInfo.body.mqttEndpoint,
+		mqttTopicPrefix: accountInfo.body.mqttTopicPrefix,
+		tenantId,
+		accountDeviceClientId: `account-${tenantId}`,
+	}
+}
+
+async function getNrfcloudCredentialsSSM(): Promise<
+	Nullable<CertificateCredentials>
+> {
+	const nrfcloudClientCertResponse = await SSM.send(
+		new GetParameterCommand({
+			Name: `/${STACK_NAME}/${NRFCLOUD_CLIENT_CERT_PARAM}`,
+		}),
+	).catch(() => null)
+	const nrfcloudClientKeyResponse = await SSM.send(
+		new GetParameterCommand({
+			Name: `/${STACK_NAME}/${NRFCLOUD_CLIENT_KEY_PARAM}`,
+		}),
+	).catch(() => null)
+
+	return {
+		clientCert: nrfcloudClientCertResponse?.Parameter?.Value,
+		privateKey: nrfcloudClientKeyResponse?.Parameter?.Value,
+	}
+}
+
+async function generateNrfcloudCredentials({
+	apiKey,
+	endpoint,
+}: {
+	apiKey: string
+	endpoint: string
+}): Promise<CertificateCredentials> {
+	const accountDevice = await needle(
+		'post',
+		`${endpoint}/v1/devices/account`,
+		null,
+		{
+			json: true,
+			headers: {
+				Authorization: `Bearer ${apiKey}`,
+			},
+		},
+	)
+
+	return {
+		clientCert: accountDevice.body.clientCert,
+		privateKey: accountDevice.body.privateKey,
+	}
+}
+
+async function deleteNrfcloudCredentials({
+	apiKey,
+	endpoint,
+}: {
+	apiKey: string
+	endpoint: string
+}): Promise<void> {
+	await needle('delete', `${endpoint}/v1/devices/account`, null, {
+		json: true,
+		headers: {
+			Authorization: `Bearer ${apiKey}`,
+		},
+	})
+}
+
+async function saveNrfcloudCredentialsSSM(
+	credentials: CertificateCredentials,
+): Promise<void> {
+	await Promise.all([
+		SSM.send(
+			new PutParameterCommand({
+				Name: `/${STACK_NAME}/${NRFCLOUD_CLIENT_CERT_PARAM}`,
+				Value: credentials.clientCert,
+				Type: ParameterType.STRING,
+				Overwrite: true,
+			}),
+		),
+		SSM.send(
+			new PutParameterCommand({
+				Name: `/${STACK_NAME}/${NRFCLOUD_CLIENT_KEY_PARAM}`,
+				Value: credentials.privateKey,
+				Type: ParameterType.STRING,
+				Overwrite: true,
+			}),
+		),
+	])
+}
+
+async function saveNrfcloudAccountInfosSSM(info: AccountInfo): Promise<void> {
+	await SSM.send(
+		new PutParameterCommand({
+			Name: `/${STACK_NAME}/${NRFCLOUD_ACCOUNT_INFO_PARAM}`,
+			Value: JSON.stringify(info),
+			Type: ParameterType.STRING,
+			Overwrite: true,
+		}),
+	)
+}
+
+export {
+	getAccountInfo,
+	getNrfcloudCredentialsSSM,
+	generateNrfcloudCredentials,
+	deleteNrfcloudCredentials,
+	saveNrfcloudCredentialsSSM,
+	saveNrfcloudAccountInfosSSM,
+}
