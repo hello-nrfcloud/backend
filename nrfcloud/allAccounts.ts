3a7d1dd24b51f55d2b8de367d27efa123d5faf75
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-07-20 23:04:06 +0200

    fix: return empty object if no setting

--- nrfcloud/allAccounts.ts
+++ nrfcloud/allAccounts.ts
@@ -1,5 +1,5 @@
 import type { SSMClient } from '@aws-sdk/client-ssm'
-import { Scope, getSettings } from '../util/settings.js'
+import { Scope, getSettingsOptional } from '../util/settings.js'
 
 export const allAccountScopes = [Scope.EXEGER_CONFIG, Scope.NODIC_CONFIG]
 
@@ -11,20 +11,20 @@ export const getAllAccountsSettings =
 		ssm: SSMClient
 		stackName: string
 	}) =>
-	async (): Promise<Record<string, T>> =>
+	async (): Promise<Record<string, T | Record<string, string>>> =>
 		allAccountScopes.reduce(
 			async (resultPromise, scope) => {
 				const result = await resultPromise
 				const account = scope.toString().replace(/([^/]+\/)/, '')
-				const settings = await getSettings<T>({
+				const settings = await getSettingsOptional<T, Record<string, string>>({
 					ssm,
 					stackName,
 					scope,
-				})()
+				})({})
 				return {
 					...result,
 					[account]: settings,
 				}
 			},
-			Promise.resolve({} as Record<string, T>),
+			Promise.resolve({} as Record<string, T | Record<string, string>>),
 		)
