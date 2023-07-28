e445558a682d39586e17339abd0fc6b4ad216b3e
Markus Tacker <m@coderbyheart.com>
2023-07-28 13:28:19 +0200

    test(e2e): preserve result

--- feature-runner/console-reporter.ts
+++ feature-runner/console-reporter.ts
@@ -1,18 +1,27 @@
 import {
-	type SuiteResult,
 	consoleReporter,
+	type SuiteResult,
 } from '@nordicsemiconductor/bdd-markdown'
+
+const onlyFailed = process.argv.includes('--only-failed')
+const withTimestamps = process.argv.includes('--with-timestamps')
 
 const chunks: string[] = []
 
-process.stdin.on('data', (data) => {
-	chunks.push(data.toString())
+process.stdin.on('data', (chunk) => chunks.push(chunk.toString()))
+
+await new Promise((resolve) => process.stdin.on('end', resolve))
+
+let res: SuiteResult
+try {
+	res = JSON.parse(chunks.join(''))
+} catch (error) {
+	throw new Error(`Failed to parse result JSON: ${(error as Error).message}`)
+}
+
+consoleReporter(res, console.log, {
+	onlyFailed,
+	withTimestamps,
 })
-
-const res = await new Promise<SuiteResult>((resolve) =>
-	process.stdin.on('end', () => resolve(JSON.parse(chunks.join('')))),
-)
-
-consoleReporter(res, console.log, { withTimestamps: true })
 
 if (!res.ok) process.exit(1)
