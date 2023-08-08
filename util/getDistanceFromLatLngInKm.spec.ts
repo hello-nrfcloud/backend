5a411d07dbf25e348e44abeb75c4586fd259ef07
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-08-08 14:40:51 +0200

    fix: change distance in test from small dist to Trondheim-Oslo

--- util/getDistanceFromLatLngInKm.spec.ts
+++ util/getDistanceFromLatLngInKm.spec.ts
@@ -7,14 +7,13 @@ describe('getDistanceFromLatLngInKm()', () => {
 			lng: 10.43763831347703,
 			ts: 1691114567,
 		}
-
 		const pointB = {
-			lat: 63.42161345025134,
-			lng: 10.436310829032905,
+			lat: 59.92117247790821,
+			lng: 10.688614657210739,
 			ts: 1691114667,
 		}
 		expect(getDistanceFromLatLngInKm({ pointA, pointB })).toEqual(
-			0.0939499540663425,
+			389.52247455218924,
 		)
 	})
 	it('should return 0 if the coordinate is the same', () => {
