00493f221dfc5dc27bea8da3aee03300891fc4b8
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-11-06 22:52:58 +0100

    test: add check message from websocket

---
+++ util/checkMessageFromWebsocket.spec.ts
@@ -0,0 +1,119 @@
+import {
+	ValidateResponse,
+	checkMessageFromWebsocket,
+} from './checkMessageFromWebsocket.js'
+import { WebSocketServer, type AddressInfo } from 'ws'
+import * as net from 'node:net'
+import { setTimeout } from 'node:timers/promises'
+import { DeferTimeoutError } from './defer.js'
+
+type ConnectionError = {
+	code: string
+}
+
+const getRandomPort = async (): Promise<number> => {
+	const server = net.createServer()
+	return new Promise((resolve) => {
+		server.listen(0, () => {
+			const port = (server.address() as AddressInfo).port
+			server.close(() => {
+				resolve(port)
+			})
+		})
+	})
+}
+
+let port: number
+let fakeServer: WebSocketServer
+
+describe('checkMessageFromWebsocket', () => {
+	beforeAll(async () => {
+		port = await getRandomPort()
+		fakeServer = new WebSocketServer({ port })
+	})
+
+	beforeEach(() => {
+		jest.clearAllMocks()
+	})
+
+	afterAll(() => {
+		fakeServer.close()
+	})
+
+	it('should resolve with true on successful validation', async () => {
+		const validate = jest.fn().mockResolvedValue(ValidateResponse.valid)
+		const result = await checkMessageFromWebsocket({
+			endpoint: `ws://localhost:${port}`,
+			timeoutMS: 1000,
+			onConnect: async () => {
+				fakeServer.clients.forEach((client) => {
+					client.send('hello')
+				})
+			},
+			validate,
+		})
+
+		expect(result).toBe(true)
+		expect(validate).toHaveBeenCalled()
+	})
+
+	it('should reject with false on invalid message', async () => {
+		const validate = jest.fn().mockResolvedValue(ValidateResponse.invalid)
+
+		try {
+			await checkMessageFromWebsocket({
+				endpoint: `ws://localhost:${port}`,
+				timeoutMS: 1000,
+				onConnect: async () => {
+					fakeServer.clients.forEach((client) => {
+						client.send('hello')
+					})
+				},
+				validate,
+			})
+		} catch (error) {
+			expect(error).toBe(false)
+			expect(validate).toHaveBeenCalled()
+		}
+	})
+
+	it('should reject with DeferTimeoutError on timeout', async () => {
+		const validate = jest.fn().mockResolvedValue(ValidateResponse.invalid)
+
+		try {
+			await checkMessageFromWebsocket({
+				endpoint: `ws://localhost:${port}`,
+				timeoutMS: 500,
+				onConnect: async () => {
+					await setTimeout(600)
+					fakeServer.clients.forEach((client) => {
+						client.send('hello')
+					})
+				},
+				validate,
+			})
+		} catch (error) {
+			expect(error).toBeInstanceOf(DeferTimeoutError)
+		}
+	})
+
+	it('should reject on WebSocket error', async () => {
+		const validate = jest.fn().mockResolvedValue(ValidateResponse.invalid)
+
+		try {
+			await checkMessageFromWebsocket({
+				endpoint: `ws://localhost:${port + 1}`,
+				timeoutMS: 500,
+				onConnect: async () => {
+					await setTimeout(600)
+					fakeServer.clients.forEach((client) => {
+						client.send('hello')
+					})
+				},
+				validate,
+			})
+		} catch (error) {
+			expect((error as ConnectionError).code).toEqual('ECONNREFUSED')
+		}
+	})
+})
