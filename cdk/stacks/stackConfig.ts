21b0cf2372c723bf11c2ac46f32b7018a1663ca9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-11-13 18:20:05 +0100

    ci: do not re-use STACK_NAME

--- cdk/stacks/stackConfig.ts
+++ cdk/stacks/stackConfig.ts
@@ -1,5 +1,5 @@
 export const STACK_NAME = process.env.STACK_NAME ?? 'hello-nrfcloud-backend'
-export const CI_STACK_NAME = process.env.STACK_NAME ?? `${STACK_NAME}-ci`
+export const CI_STACK_NAME = `${STACK_NAME}-ci`
 export const TEST_RESOURCES_STACK_NAME = `${STACK_NAME}-test`
 
 // The container registry is shared between the test and the production stack
