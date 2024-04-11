0b68c149e2315290beea7362892efeb14c26b0a2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-11 20:05:54 +0200

    refactor: type layer dependencies
    [skip ci]

--- cdk/layers/baseLayer.ts
+++ cdk/layers/baseLayer.ts
@@ -2,21 +2,24 @@ import {
 	packLayer,
 	type PackedLayer,
 } from '@bifravst/aws-cdk-lambda-helpers/layer'
+import pJson from '../../package.json'
+
+const dependencies: Array<keyof (typeof pJson)['dependencies']> = [
+	'@nordicsemiconductor/from-env',
+	'@nordicsemiconductor/timestream-helpers',
+	'@sinclair/typebox',
+	'@hello.nrfcloud.com/proto',
+	'p-limit',
+	'@aws-lambda-powertools/metrics',
+	'lodash-es',
+	'@middy/core',
+	'@hello.nrfcloud.com/lambda-helpers',
+	'@bifravst/aws-ssm-settings-helpers',
+	'@hello.nrfcloud.com/nrfcloud-api-helpers',
+]
 
 export const pack = async (): Promise<PackedLayer> =>
 	packLayer({
 		id: 'baseLayer',
-		dependencies: [
-			'@nordicsemiconductor/from-env',
-			'@nordicsemiconductor/timestream-helpers',
-			'@sinclair/typebox',
-			'@hello.nrfcloud.com/proto',
-			'p-limit',
-			'@aws-lambda-powertools/metrics',
-			'lodash-es',
-			'@middy/core',
-			'@hello.nrfcloud.com/lambda-helpers',
-			'@bifravst/aws-ssm-settings-helpers',
-			'@hello.nrfcloud.com/nrfcloud-api-helpers',
-		],
+		dependencies,
 	})
--- cdk/layers/healthCheckLayer.ts
+++ cdk/layers/healthCheckLayer.ts
@@ -2,9 +2,16 @@ import {
 	packLayer,
 	type PackedLayer,
 } from '@bifravst/aws-cdk-lambda-helpers/layer'
+import pJson from '../../package.json'
+
+const dependencies: Array<keyof (typeof pJson)['dependencies']> = [
+	'mqtt',
+	'ws',
+	'@hello.nrfcloud.com/lambda-helpers',
+]
 
 export const pack = async (): Promise<PackedLayer> =>
 	packLayer({
 		id: 'healthCheckLayer',
-		dependencies: ['mqtt', 'ws', '@hello.nrfcloud.com/lambda-helpers'],
+		dependencies,
 	})
