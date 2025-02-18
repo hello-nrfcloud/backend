df0385af2ed704a7b1a8864ad8fc15921204df32
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-02-18 01:13:22 +0100

    test: add more test case

--- lambda/fota/getNextUpgrade.spec.ts
+++ lambda/fota/getNextUpgrade.spec.ts
@@ -1,16 +1,13 @@
 import { FOTAJobTarget } from '@hello.nrfcloud.com/proto/hello'
 import assert from 'node:assert/strict'
-import { describe, it } from 'node:test'
+import test, { describe, it } from 'node:test'
 import { getNextUpgrade } from './getNextUpgrade.ts'
 
 void describe('getNextUpgrade()', () => {
 	void it('should return the next upgrade based on a static version', () =>
 		assert.deepEqual(
 			getNextUpgrade(
-				{
-					'2.0.0': 'APP*1e29dfa3*v2.0.1',
-					'2.0.1': 'APP*cd5412d9*v2.0.2',
-				},
+				{ '2.0.0': 'APP*1e29dfa3*v2.0.1', '2.0.1': 'APP*cd5412d9*v2.0.2' },
 				{
 					appVersion: '2.0.0',
 					supportedFOTATypes: [FOTAJobTarget.application],
@@ -26,22 +23,44 @@ void describe('getNextUpgrade()', () => {
 			},
 		))
 
-	void it('should return the next upgrade based on a range', () =>
+	void describe('should return the next upgrade based on a range', () => {
+		for (const range of ['>=1.0.0', '<2.0.1']) {
+			void test(range, () =>
+				assert.deepEqual(
+					getNextUpgrade(
+						{ [range]: 'APP*1e29dfa3*v2.0.1' },
+						{
+							appVersion: '2.0.0',
+							supportedFOTATypes: [FOTAJobTarget.application],
+							mfwVersion: '1.3.6',
+						},
+					),
+					{
+						upgrade: {
+							reportedVersion: '2.0.0',
+							bundleId: 'APP*1e29dfa3*v2.0.1',
+							target: FOTAJobTarget.application,
+						},
+					},
+				),
+			)
+		}
+	})
+
+	void it('should return null in case of no upgrade', () =>
 		assert.deepEqual(
 			getNextUpgrade(
+				{ '<2.0.1': 'APP*1e29dfa3*v2.0.1' },
 				{
-					'>=1.0.0': 'APP*1e29dfa3*v2.0.1',
-				},
-				{
-					appVersion: '2.0.0',
+					appVersion: '2.0.1',
 					supportedFOTATypes: [FOTAJobTarget.application],
 					mfwVersion: '1.3.6',
 				},
 			),
 			{
 				upgrade: {
-					reportedVersion: '2.0.0',
-					bundleId: 'APP*1e29dfa3*v2.0.1',
+					reportedVersion: '2.0.1',
+					bundleId: null,
 					target: FOTAJobTarget.application,
 				},
 			},
